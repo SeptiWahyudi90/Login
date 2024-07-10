eee9fac114615642d2e9cb80e3c0bbf050e47e0b
SeptiWahyudi90 <septiwahyudi90@gmail.com>
2024-07-10 10:51:15 +0700

    Initial commit

---
+++ app/src/main/java/com/pma212102375/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.pma212102375.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
